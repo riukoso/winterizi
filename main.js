4509c5fc4c6172e342924741b49e0fb98d189a0a
riukoso <ttitorm@gmail.com>
2022-03-24 20:54:50 -0500

    Adding if to buttons

--- main.js
+++ main.js
@@ -3,8 +3,12 @@ const rules = document.getElementsByClassName('rules')[0]
 const stanlist = document.getElementsByClassName('stanlist')[0]
 
 const show =(element)=>{
-    about.classList.remove("display");
-    rules.classList.remove("display");
-    stanlist.classList.remove("display");
-    element.classList.add("display");
+    if(element.classList.contains('display')){
+        element.classList.remove("display")
+    }else{
+        about.classList.remove("display");
+        rules.classList.remove("display");
+        stanlist.classList.remove("display");
+        element.classList.add("display");
+    }
 };
